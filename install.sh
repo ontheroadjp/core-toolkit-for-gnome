e0471eb8fe3dc0b219d0d0d8f8e67b64273e9667
ontheroadjp <dev@ontheroad.jp>
2026-06-16 23:40:20 +0900

    refactor: relocate dotfiles under root/ mirror, automate keyd and gnome-overview-toggle keybinding
    Alacritty and gnome-overview-toggle dotfiles now live under root/home/user/...
    to mirror their absolute target paths, and install.sh derives the repo path
    via `ghq root` instead of a hardcoded path. install.sh also adds keyd config
    symlinking and registers the gnome-overview-toggle custom keybinding via
    gsettings, automating a step that was previously manual.

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-ln -sf $(ghq root)/github.com/ontheroadjp/core-toolkit-for-gnome/.local/bin/gnome-overview-toggle $HOME/.local/bin
-ln -sf $(ghq root)/github.com/ontheroadjp/core-toolkit-for-gnome/.config/alacritty $HOME/.config
-ln -sf $(ghq root)/github.com/ontheroadjp/core-toolkit-for-gnome/.local/share/applications/Alacritty.desktop $HOME/.local/share/applications
+
+CORE_TOOLKIT_FOR_GNOME_PATH="$(ghq root)/github.com/ontheroadjp/core-toolkit-for-gnome"
+
+# ----------------------------------
+# alacritty
+# ----------------------------------
+ln -sf ${CORE_TOOLKIT_FOR_GNOME_PATH}/root/home/user/.local/share/applications/Alacritty.desktop $HOME/.local/share/applications
+ln -sf ${CORE_TOOLKIT_FOR_GNOME_PATH}/root/home/user/.config/alacritty $HOME/.config
+
+# ----------------------------------
+# keyd
+# ----------------------------------
+sudo ln -sf ${CORE_TOOLKIT_FOR_GNOME_PATH}/root/etc/keyd /etc
+
+# ----------------------------------
+# settings > keyboard > custom shortcuts
+# ----------------------------------
+GNOME_OVERVIEW_TOGGLE_PATH="${HOME}/.local/bin/gnome-overview-toggle"
+
+ln -sf ${CORE_TOOLKIT_FOR_GNOME_PATH}/root/home/user/.local/bin/gnome-overview-toggle ${GNOME_OVERVIEW_TOGGLE_PATH}
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'gnome-overview-toggle'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "${GNOME_OVERVIEW_TOGGLE_PATH}"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Shift>space'
+
+exit 0
