bb1282a18c36d9cc0d9461ec25cf27d2cc84d65a
ontheroadjp <dev@ontheroad.jp>
2026-06-16 17:54:52 +0900

    feat: add install.sh for dotfile symlinks and Alacritty desktop entry
    Symlinks gnome-overview-toggle, alacritty config, and the Alacritty
    .desktop entry into the home directory.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -sf $(ghq root)/github.com/ontheroadjp/core-toolkit-for-gnome/.local/bin/gnome-overview-toggle $HOME/.local/bin
+ln -sf $(ghq root)/github.com/ontheroadjp/core-toolkit-for-gnome/.config/alacritty $HOME/.config
+ln -sf $(ghq root)/github.com/ontheroadjp/core-toolkit-for-gnome/.local/share/applications/Alacritty.desktop $HOME/.local/share/applications
