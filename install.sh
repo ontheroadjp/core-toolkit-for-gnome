2a78844ab31130a84be17f1fef7494c6ff9ad694
ontheroadjp <dev@ontheroad.jp>
2026-06-21 20:26:38 +0900

    chore(install.sh): comment out gnome-overview-toggle keybinding setup
    - Disable symlink creation and gsettings registration for gnome-overview-toggle
    - Custom shortcut binding `<Control><Shift>space` is no longer applied on install

--- install.sh
+++ install.sh
@@ -21,12 +21,12 @@ ln -sf ${CORE_TOOLKIT_FOR_GNOME_PATH}/root/home/user/.config/mpv $HOME/.config
 # ----------------------------------
 # settings > keyboard > custom shortcuts
 # ----------------------------------
-GNOME_OVERVIEW_TOGGLE_PATH="${HOME}/.local/bin/gnome-overview-toggle"
-
-ln -sf ${CORE_TOOLKIT_FOR_GNOME_PATH}/root/home/user/.local/bin/gnome-overview-toggle ${GNOME_OVERVIEW_TOGGLE_PATH}
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'gnome-overview-toggle'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "${GNOME_OVERVIEW_TOGGLE_PATH}"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Shift>space'
+# GNOME_OVERVIEW_TOGGLE_PATH="${HOME}/.local/bin/gnome-overview-toggle"
+#
+# ln -sf ${CORE_TOOLKIT_FOR_GNOME_PATH}/root/home/user/.local/bin/gnome-overview-toggle ${GNOME_OVERVIEW_TOGGLE_PATH}
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'gnome-overview-toggle'
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "${GNOME_OVERVIEW_TOGGLE_PATH}"
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Shift>space'
 
 # ----------------------------------
 # fep-switcher (GNOME extension: core)
